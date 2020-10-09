87841e4e22b946cc4d588a5dbda8263d5f0955ef
alexbook00 <43623251+alexbook00@users.noreply.github.com>
2020-10-08 21:05:38 -0600

    Add files via upload

--- PS5&6/PS56_p4.py
+++ PS5&6/PS56_p4.py
@@ -12,33 +12,26 @@ from os import listdir
 # Treat these networks as simple graphs in your analysis.
 if __name__ == '__main__':
 
-    # get list of edge list files
     path = 'facebook100txt'
     edge_list_filepaths = []
     for f in listdir(path):
-        # only select edge list files
         if ('_attr' not in f) and ('readme' not in f) and ('pdf' not in f) and ('.DS_Store' not in f):
             fp = os.path.join(path, f)
-            # test if file actually exists
             if os.path.isfile(fp):
                 edge_list_filepaths.append(fp)
     edge_list_filepaths = sorted(edge_list_filepaths)
 
-    # get list of attribute files
     path = 'facebook100txt'
     attr_filepaths = []
     for f in listdir(path):
-        # only select attribute files
         if '_attr' in f:
             fp = os.path.join(path, f)
-            # test if file actually exists
             if os.path.isfile(fp):
                 attr_filepaths.append(fp)
     attr_filepaths = sorted(attr_filepaths)
 
     modList = []
 
-    # print(len(edge_list_filepaths), len(attr_filepaths))
     for i in range(len(edge_list_filepaths)):
         print(attr_filepaths[i])
 
@@ -70,8 +63,68 @@ if __name__ == '__main__':
             degree_set.append(attrDF.index[attrDF['degree'] == j].tolist())
         Q_degree = nx_qual.modularity(G, degree_set)
 
-        modList.append([attr_filepaths[i].split('_')[0].split('\\')[1], Q_status, Q_major, Q_degree])
+        network_size = len(attrDF.index)
 
-    # pprint(modList)
-    df = pd.DataFrame(modList, columns=['School', 'Modularity by status', 'Modularity by major', 'Modularity by degree'])
+        modList.append([attr_filepaths[i].split('_')[0].split('\\')[1], Q_status, Q_major, Q_degree, network_size])
+
+    df = pd.DataFrame(modList, columns=['School', 'Modularity by status', 'Modularity by major', 'Modularity by degree', 'Network size'])
     df.to_csv('df_out.csv')
+
+    dfNew = pd.read_csv('df_out.csv')
+
+    fig, ax = plt.subplots()
+    ax.scatter(dfNew['Network size'], dfNew['Modularity by status'], alpha = .9)
+    ax.hlines(0, 1e2, 1e5, alpha = .4)
+    ax.grid(True)
+    ax.set_xlim(1e2, 1e5)
+    ax.set_xlabel('Network size, n', fontsize=18)
+    ax.set_ylabel('Modularity by status, Q', fontsize=18)
+    ax.set_xscale('log')
+    plt.show()
+
+    fig, ax = plt.subplots()
+    dfNew['Modularity by status'].plot(kind='density')
+    ax.grid(True)
+    ax.set_xlabel('Modularity by status, Q', fontsize=18)
+    ax.set_ylabel('Density', fontsize=18)
+    ax.set_ylim(0, ax.get_ylim()[1])
+    ax.vlines(0, -1000, 10000, alpha = .4)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.scatter(dfNew['Network size'], dfNew['Modularity by major'], alpha = .9)
+    ax.hlines(0, 1e2, 1e5, alpha = .4)
+    ax.grid(True)
+    ax.set_xlim(1e2, 1e5)
+    ax.set_xlabel('Network size, n', fontsize=18)
+    ax.set_ylabel('Modularity by major, Q', fontsize=18)
+    ax.set_xscale('log')
+    plt.show()
+
+    fig, ax = plt.subplots()
+    dfNew['Modularity by major'].plot(kind='density')
+    ax.grid(True)
+    ax.set_xlabel('Modularity by major, Q', fontsize=18)
+    ax.set_ylabel('Density', fontsize=18)
+    ax.set_ylim(0, ax.get_ylim()[1])
+    ax.vlines(0, -1000, 10000, alpha = .4)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.scatter(dfNew['Network size'], dfNew['Modularity by degree'], alpha = .9)
+    ax.hlines(0, 1e2, 1e5, alpha = .4)
+    ax.grid(True)
+    ax.set_xlim(1e2, 1e5)
+    ax.set_xlabel('Network size, n', fontsize=18)
+    ax.set_ylabel('Modularity by node degree, Q', fontsize=18)
+    ax.set_xscale('log')
+    plt.show()
+
+    fig, ax = plt.subplots()
+    dfNew['Modularity by degree'].plot(kind='density')
+    ax.grid(True)
+    ax.set_xlabel('Modularity by node degree, Q', fontsize=18)
+    ax.set_ylabel('Density', fontsize=18)
+    ax.set_ylim(0, ax.get_ylim()[1])
+    ax.vlines(0, -1000, 10000, alpha = .4)
+    plt.show()
