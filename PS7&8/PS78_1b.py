b4ed67f857c34925fd483c6510c05e303f1e90b7
alexbook00 <43623251+alexbook00@users.noreply.github.com>
2020-10-23 00:50:38 -0600

    Add files via upload

--- PS7&8/PS78_1b.py
+++ PS7&8/PS78_1b.py
@@ -3,6 +3,7 @@ from webweb import Web
 import networkx as nx
 from pprint import pprint
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # choose the number of groups
 B = 2
@@ -21,38 +22,66 @@ omega = [
     [within,between,0],
     [between,within,between],
     [0,between,within]]
-# make that happy little SBM!
-edge_list = []
-for i in range(1,n):
-    for j in range(i):
-        if np.random.rand() < omega[group_id[i]][group_id[j]]:
-            edge_list.append([i,j])
+# # make that happy little SBM!
+# edge_list = []
+# for i in range(1,n):
+#     for j in range(i):
+#         if np.random.rand() < omega[group_id[i]][group_id[j]]:
+#             edge_list.append([i,j])
+#
+# G = nx.Graph()
+# G.add_edges_from(edge_list)
 
-G = nx.Graph()
-G.add_edges_from(edge_list)
+p_array = [.01, .02, .03, .04, .05, .06, .07, .08, .09, .1,
+           .11, .12, .13, .14, .15, .16, .17, .18, .19, .2,
+           .21, .22, .23, .24, .25, .26, .27, .28, .29, .3,
+           .4, .5, .6, .7, .8, .9]
 
+length_size_dict = {} # key is p, value is (average length, average size) tuple
 
-length_array = []
-p = 1
+for p in p_array:
 
-for i in range(100):
-    spread = True
-    S_I_list = np.array(['S']*1000)
-    l = 0
+    # each iteration is one simulation of a pandemic
+    length_array = []
+    size_array = []
 
-    while spread == True:
-        spread = False
-        if l == 0:
-            S_I_list[np.random.randint(1000)] = 'I'
+    for i in range(100):
+        # make that happy little SBM!
+        edge_list = []
+        for i in range(1,n):
+            for j in range(i):
+                if np.random.rand() < omega[group_id[i]][group_id[j]]:
+                    edge_list.append([i,j])
 
-        for node in G.nodes():
-            for neighbor in G.neighbors(node):
-                if (S_I_list[node] == 'I') and (S_I_list[neighbor] == 'S') and (np.random.uniform() < p):
-                    S_I_list[neighbor] = 'I'
-                    spread = True
+        G = nx.Graph()
+        G.add_edges_from(edge_list)
 
-        l += 1
+        spread = True
+        S_I_list = np.array(['S']*1000)
+        l = 0
+        num_infected = 0
 
-    length_array.append(l)
+        while spread == True:
+            spread = False
+            if l == 0:
+                S_I_list[np.random.randint(1000)] = 'I'
+                num_infected += 1
 
-print('Average pandemic length for p={p}: {avg}'.format(p=p, avg=np.average(length_array)))
+            for node in G.nodes():
+                for neighbor in G.neighbors(node):
+                    if (S_I_list[node] == 'I') and (S_I_list[neighbor] == 'S') and (np.random.uniform() < p):
+                        S_I_list[neighbor] = 'I'
+                        spread = True
+                        num_infected += 1
+
+            l += 1
+
+        length_array.append(l)
+        size_array.append(num_infected/len(S_I_list))
+
+    length_size_dict[p] = (np.average(length_array), np.average(size_array))
+
+# print('Average pandemic length for p={p}: {avg}'.format(p=p, avg=np.average(length_array)))
+# pprint(length_size_dict)
+df = pd.DataFrame.from_dict(length_size_dict)
+df.T.to_csv('pandemic_data.csv')
